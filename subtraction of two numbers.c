ffba3891dd5be28bc0ea0f697eaf840a0a2740fb
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-09-28 17:33:06 +0530

    Create subtraction of two numbers.c

---
+++ subtraction of two numbers.c
@@ -0,0 +1,8 @@
+int main() {
+    int number1,number2,result;
+    printf("Enter two integers:");
+    scanf("%d %d",&number1,&number2);
+    result=number1-number2;
+    printf("result:%d",result);
+    return 0;
+}
