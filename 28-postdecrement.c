38533bd4c98db086bba68754f8b4dae72248d2d2
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-10-05 23:37:35 +0530

    Create 28-postdecrement.c

---
+++ 28-postdecrement.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int i_temp_variable,j_temp_variable;
+    printf("enter the value of i_temp_variable:");
+    scanf("%d",&i_temp_variable);
+    j_temp_variable=5+i_temp_variable--;
+    printf("%d",j_temp_variable);
+    return 0;
+}
