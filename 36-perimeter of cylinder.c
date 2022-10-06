53772c241256ebedfa4cf4974a5c621ec62ba84f
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-10-06 13:49:07 +0530

    Create 36-perimeter of cylinder.c

---
+++ 36-perimeter of cylinder.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int radius,height,perimeter;
+    printf("enter the radius:");
+    scanf("%d",&radius);
+    printf("enter the height:");
+    scanf("%d",&height);
+    printf("%d",perimeter=4*radius+2*height);
+    return 0;
+}
