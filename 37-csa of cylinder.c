c1e1bf5c1862688d498e1d4743c382cd319e4431
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-10-06 13:51:21 +0530

    Create 37-csa of cylinder.c

---
+++ 37-csa of cylinder.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+float main() {
+    float radius,height,csa;
+    printf("enter the radius:");
+    scanf("%f",&radius);
+    printf("enter the height:");
+    scanf("%f",&height);
+    printf("%f",csa=2*radius*height*3.14);
+    return 0;
+}
