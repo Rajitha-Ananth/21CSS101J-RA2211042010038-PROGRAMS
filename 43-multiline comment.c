864a0b35aa4ff5d4dccddf5a988167ae1ce5c4c3
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-10-06 21:33:44 +0530

    Create 43-multiline comment.c

---
+++ 43-multiline comment.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+int i,cube; 
+printf("enter a number");
+scanf("%d",&i);
+/*the formula for cube of a number is number*number*number*/
+cube=i*i*i;
+printf("%d",cube);
+return 0;
+}
