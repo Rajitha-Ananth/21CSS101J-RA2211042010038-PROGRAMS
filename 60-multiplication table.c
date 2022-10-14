7f1f5ceec70246ea41a79f5aa2e3f56b8717a114
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-10-14 19:30:20 +0530

    Update 60-multiplication table.c

--- 60-multiplication table.c
+++ 60-multiplication table.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main() {
-  int n, i;
+  printf("RA2211042010038\n");
+  int n,i;
   printf("Enter an integer: ");
-  scanf("%d", &n);
-  for (i = 1; i <= 10; ++i) {
-    printf("%d * %d = %d \n", n, i, n * i);
+  scanf("%d",&n);
+  for (i=1;i<=10;++i) {
+    printf("%d*%d=%d\n",n,i,n*i);
   }
   return 0;
 }
