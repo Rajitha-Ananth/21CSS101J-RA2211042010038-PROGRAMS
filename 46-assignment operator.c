cdfc59bf14d1861b47271c8ca7dac5fc6d11efc6
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-10-14 19:10:09 +0530

    Update 46-assignment operator.c

--- 46-assignment operator.c
+++ 46-assignment operator.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 int main() {
+    printf("RA2211042010038\n");
     int x = 10;
     int y = x;
-    
-    printf("x = %d\n" , x); 
-    printf("y = %d\n" , y); 
+    printf("x = %d\n",x); 
+    printf("y = %d\n",y); 
 }
