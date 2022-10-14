749b4cb08c1a96e89095f774b628ccc3320bd07c
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-10-14 13:18:15 +0530

    Update 20-equal .c

--- 20-equal .c
+++ 20-equal .c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
 int main() {
+  printf("RA2211042010038\n");
   int x = 8;
   int y = 5;
-  printf("%d", x == y); // returns 0 (false) because 8 is not equal to 3
+  printf("%d", x == y);
   return 0;
 }
