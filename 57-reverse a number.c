7e8e032b9e6e85458d5663bc3619418150ac45c2
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-10-14 19:23:19 +0530

    Update 57-reverse a number.c

--- 57-reverse a number.c
+++ 57-reverse a number.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 int main() {
-  int n, reverse = 0, remainder;
-
+  printf("RA2211042010038\n");
+  int n,reverse=0,remainder;
   printf("Enter an integer: ");
-  scanf("%d", &n);
-  while (n != 0) {
-    remainder = n % 10;
-    reverse = reverse * 10 + remainder;
-    n /= 10;
+  scanf("%d",&n);
+  while (n!=0) {
+    remainder=n%10;
+    reverse=reverse*10+remainder;
+    n/=10;
   }
-  printf("Reversed number = %d", reverse);
-
+  printf("Reversed number=%d",reverse);
   return 0;
 }
