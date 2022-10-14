3572cbb4ec240f83ff8b2c34f674031175aa83da
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-10-14 13:30:44 +0530

    Update 27- vowel.c

--- 27- vowel.c
+++ 27- vowel.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 char main() {
+    printf("RA2211042010038\n");
     char c;
     printf("enter the alphabet");
     scanf("%c",&c);
     switch(c)
     {
         case 'a':
-        printf("\n\n entered alphabet is a vowel");
+        printf("\n\n enterd alphabet is a vowel");
         scanf("%c",&c);
         break;
         case 'e':
-        printf("\n\n entered alphabet is a vowel");
+        printf("\n\n enterd alphabet is a vowel");
         scanf("%c",&c);
         break;
         case 'i':
