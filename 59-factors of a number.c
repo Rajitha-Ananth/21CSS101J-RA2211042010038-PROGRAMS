d54fbc82893cf26c269894c76b3c5d3515be84f9
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-10-14 19:29:26 +0530

    Update 59-factors of a number.c

--- 59-factors of a number.c
+++ 59-factors of a number.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main() {
+    printf("RA2211042010038\n");
     int num, i;
     printf("Enter a positive integer: ");
-    scanf("%d", &num);
-    printf("Factors of %d are: ", num);
-    for (i = 1; i <= num; ++i) {
-        if (num % i == 0) {
-            printf("%d ", i);
+    scanf("%d",&num);
+    printf("Factors of %d are:", num);
+    for (i =1;i<=num;++i) {
+        if (num%i==0) {
+            printf("%d",i);
         }
     }
     return 0;
