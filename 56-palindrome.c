8c00d678e6714134250fd5c7de09a2a1998ba3c1
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-10-14 19:21:45 +0530

    Update 56-palindrome.c

--- 56-palindrome.c
+++ 56-palindrome.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 int main() {
+  printf("RA2211042010038\n");
   int n, reversed = 0, remainder, original;
     printf("Enter an integer: ");
-    scanf("%d", &n);
-    original = n;
-    while (n != 0) {
-        remainder = n % 10;
-        reversed = reversed * 10 + remainder;
-        n /= 10;
+    scanf("%d",&n);
+    original=n;
+    while (n!= 0) {
+        remainder = n%10;
+        reversed = reversed*10+remainder;
+        n/=10;
     }
-    if (original == reversed)
-        printf("%d is a palindrome.", original);
+    if(original == reversed)
+       printf("%d is a palindrome",original);
     else
-        printf("%d is not a palindrome.", original);
-
+        printf("%d is not a palindrome.",original);
     return 0;
 }
