9ba4c189e38ed7b833a8796f5d6fadc22e8c6faa
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-10-14 19:25:21 +0530

    Update 58-armstrong number.c

--- 58-armstrong number.c
+++ 58-armstrong number.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
  int main() {
-    int num, originalNum, remainder, result = 0;
-    printf("Enter a three-digit integer: ");
-    scanf("%d", &num);
-    originalNum = num;
-    while (originalNum != 0) {
-        remainder = originalNum % 10;
-       result += remainder * remainder * remainder;
-       originalNum /= 10;
+  printf("RA2211042010038\n");
+    int num,originalnumber,remainder,result=0;
+    printf("Enter a three digit integer:");
+    scanf("%d",&num);
+    originalnumber=num;
+    while(originalNum!= 0) {
+        remainder=originalnumber%10;
+       result+=remainder*remainder*remainder;
+       originalnumber/=10;
     }
-    if (result == num)
-        printf("%d is an Armstrong number.", num);
+    if(result == num)
+        printf("%d is an armstrong number", num);
     else
-        printf("%d is not an Armstrong number.", num);
+        printf("%d is not an armstrong number", num);
     return 0;
 }
