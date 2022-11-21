2c54753d106831b8c9cdcc810c7749b3d4577708
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-21 23:58:15 +0530

    Create 95-output based print.c

---
+++ 95-output based print.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+	int row, j;
+  printf("RA2211042010038\n"
+	for(row=1;row<=5;row++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			printf(“%d”,j);
+		}
+		printf(“\n”);
+	}
+	return 0;
+}
