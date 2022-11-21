e79ddfd2947dcd702ce46da531954f59d22b794a
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-21 11:03:22 +0530

    Create 78-nested else.c

---
+++ 78-nested else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main() {
+	int a, b, c;
+	printf("Enter three integer values : ");
+	scanf("%d %d %d", &a, &b, &c);
+	if (a>b && a>c) {
+		printf("%d is greater than %d and %d\n",a,b,c);
+	} 
+	else if(b>a && b>c ) {
+		printf("%d is greater than %d and %d\n",b,a,c);
+	}
+	else {
+		printf("%d is greater than %d and %d\n",c,a,b);
+	}
+}
