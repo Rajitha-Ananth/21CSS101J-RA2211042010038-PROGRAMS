f8b8da37f52e327fb6bc20b74120e468935addf0
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-21 09:52:55 +0530

    Create 77-checking smallest and the largest.ct.c

---
+++ 77-checking smallest and the largest.ct.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main() {
+  int a,b ;
+  printf("enter two number : );
+  scanf("%d%d",&a,&b);
+  if (a>b) {
+    printf("the smallest is:",b);
+    printf("the largest is :",a);
+  }
+ else {
+   printf("the smallest is:",b);
+   printf("the largest is :",a);
+ }
+    
