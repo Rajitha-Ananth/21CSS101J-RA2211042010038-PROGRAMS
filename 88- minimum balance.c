b45efd7f9891119894a14dab5c55a9ea1d17f160
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-21 21:34:56 +0530

    Create 88- minimum balance.c

---
+++ 88- minimum balance.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+float main() {
+	float balance,minimum_balance=1000;
+	printf("Enter balance : ");
+	scanf("%f",&balance);
+	if (balance>minimum_balance)
+	{
+		printf("Sufficient balance\n");
+	}
+	else {
+		printf("Balance is low\n");
+	}
+	return 0;
+}
