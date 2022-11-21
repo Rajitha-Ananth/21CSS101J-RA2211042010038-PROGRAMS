87bdc1055f610d30a2f364e318af863ff6336331
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-21 23:45:44 +0530

    Create 92-Rebatable amount.c

---
+++ 92-Rebatable amount.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main() {
+    int x,y;
+    printf("RA2211042010038\n");
+    printf("enter the maximum_rebatable amount and amount_required\t");
+    scanf("%d%d",&x,&y);
+    if (y<=x) {
+        printf("rebatable amount is :%d",y);
+    }
+    else {
+        printf("");
+    }
+    return 0;
+    
