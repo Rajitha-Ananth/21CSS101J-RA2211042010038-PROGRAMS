fb2f2b1760794967247c936af9b7d73b6af9a158
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-21 21:44:36 +0530

    Update 82- Adding two different datatypes.c

--- 82- Adding two different datatypes.c
+++ 82- Adding two different datatypes.c
@@ -2,6 +2,7 @@
 void main() {
 	int intValue;
 	float floatValue;
+	printf("RA2211042010038\n");
 	printf("Enter an int and a float value : ");
 	scanf("%d%f",&intValue,&floatValue);
 	printf("Sum of %d and %f = %f\n",intValue,floatValue,intValue+floatValue); 
