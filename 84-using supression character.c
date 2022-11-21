83ce88ac7dcbf58d8b24ff3e1b7d59a7d03441f2
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-21 21:48:24 +0530

    Update 84-using supression character.c

--- 84-using supression character.c
+++ 84-using supression character.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 void main() {
 	int a, b;
+	printf("RA2211042010038\n");
 	printf("Enter an integer : ");
 	scanf("%3d%*2d%2d", &a, &b); 
 	printf("The values in a and b are : %d, %d\n", a, b);
