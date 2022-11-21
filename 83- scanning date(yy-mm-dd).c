4346bb06c7193c8fe9a2e51f02033cfd38969d92
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-21 21:47:54 +0530

    Update 83- scanning date(yy-mm-dd).c

--- 83- scanning date(yy-mm-dd).c
+++ 83- scanning date(yy-mm-dd).c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 void main() {
 	int year,month,date,DOB;
+	printf("RA2211042010038\n");
 	printf("Enter DOB in yyyy.mm.dd format : ");
 	scanf("%d.%d.%d",&year,&month,&date);
 	printf("DOB is : %d.%d.%d\n",year,month,date);	
