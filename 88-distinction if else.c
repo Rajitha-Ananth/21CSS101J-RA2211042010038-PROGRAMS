07dab77c2a2c88fb85e9415fae39e10c7d844d86
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-21 21:49:29 +0530

    Update and rename 87-distinction if else.c to 88-distinction if else.c

--- 88-distinction if else.c
+++ 88-distinction if else.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 void main() {
 	int marks, distinction_marks = 75;
+	printf("RA2211042010038\n");
 	printf("Enter marks : ");
 	scanf("%d", &marks);
 	 if(marks>distinction_marks) { 
