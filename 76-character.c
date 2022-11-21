a703175867de7f35fc7fcd28c3b39f9f778737c4
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-21 09:33:40 +0530

    Create 76-character.c

---
+++ 76-character.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void man() {
+  char character;
+  printf("enter a character");
+  scanf("%c",&chaaracter);
+  if(character>='a' && character<='z' || character>='A' && character<='Z') {
+    printf("The character is an alphabet")
+           }
+  else {
+    printf("the character is not an alphabet")
+    }
