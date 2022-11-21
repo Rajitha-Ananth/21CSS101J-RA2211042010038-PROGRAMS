80c160596e47a1002ea49aa75056de0e60d1f8e2
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-21 13:28:52 +0530

    Create 79-min max using nested else if.c

---
+++ 79-min max using nested else if.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+ int main() {
+ 	int a,b,c,max,min;
+ 	printf("Enter 3 numbers : ");
+ 	scanf("%d%d%d",&a,&b,&c);
+ 	if(a>b && a>c) {
+ 		max=a;
+ 		}
+ 	else if(b>c && b>a) {
+ 		max=b;
+ 		}
+ 	else {
+ 		max=c;
+ 	}
+ 	if(a<b && c<b) {
+ 		min=a;
+ 	}
+ 	else if(b<c && b<a) {
+ 		min=b;
+ 	}
+ 	else {
+ 		min=c;
+ 	}
+ 		printf("The max and min of three numbers are :",max,min);
+ 		return 0;
+ 	}
+ 	
