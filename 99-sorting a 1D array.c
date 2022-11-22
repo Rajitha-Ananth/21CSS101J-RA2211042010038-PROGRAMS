a65d9979ca06c4940c9f4f5dccfeba6d85951c6e
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-22 12:04:58 +0530

    Create 99-sorting a 1D array.c

---
+++ 99-sorting a 1D array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>  
+int main( ) {
+  int i, j, temp, n, a[10];
+  printf(“enter the number of elements:”);  
+  scanf(“%d”, &n);
+  printf(“enter the elements to be Sorted\n”);
+  for(i=0; i<n; i++)
+  {
+    scanf(“%d\n”, &a[i]);
+  }
+  for(i=0; i<n; i++)
+  {
+    for(j=i+1; j<n; j++)
+    {
+      if(a[i] >a[j])
+      {
+        temp = a[i];  
+        a[i] = a[j];  
+        a[j] = temp;
+      }
+    }
+  }
+  print(“The sorted elements are: \n”);  
+  for(i=0; i<n; i++)
+  {
+    printf(“%d\n”, a[i]);
+  }
+  return 0;
+}
