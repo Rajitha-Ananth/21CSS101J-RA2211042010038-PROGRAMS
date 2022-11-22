b6db2e865e2560c7998a775ed1c791f6df12a9cd
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-22 11:58:48 +0530

    Create 97-displaying user inputted array.c

---
+++ 97-displaying user inputted array.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+ int main( ) {
+   int i, n, arr[5];
+   printf("RA2211042010038\n");
+   scanf(“%d”, &n);
+   printf(“enter the elements of erray\n”);
+   for(i=0; i<n; i++) 
+   {
+     scanf(“%d”, &a[i]);
+   }
+   printf(“ehe elements of the erray are”\n”);
+   for(i=0; i<n; i++)
+   {
+     printf(“%d”, a[i]);
+   }
+   return 0;
+ }
+//enter the elements of the erray  10 20 30 40 50
+//ehe elements of the erray are  10 20 30 40 50
+
