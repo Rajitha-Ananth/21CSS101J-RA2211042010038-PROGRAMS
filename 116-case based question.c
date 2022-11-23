b61b28f754116cc1a2620377a19d4a48f838b47e
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-23 22:27:11 +0530

    Create 116-case based question.c

---
+++ 116-case based question.c
@@ -0,0 +1,19 @@
+/* class of n students take exam on m subjects.program to read the marks and compute thier total marks*/
+#include<stdio.h>
+void main() {
+  int n,m,i,j;
+  int marks,roll_number,total;
+  printf("enter number of students and subjects");
+  scanf("%d%d",&n,&m);
+  for(i=0;i<=n;i++) {
+      printf("enter the roll_number");
+      scanf("%d",&roll_number);
+      total=0;
+      for(j=0;j<=m;j++); {
+          printf("enter the marks%d:",m);
+          scanf("%d",&marks);
+          total=total+marks;
+  }
+  printf("total marks=%d",total);
+  }
+}
