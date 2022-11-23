43261f002751a38fd190d6f2a46e5beed67a23a1
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-24 01:49:55 +0530

    Create 136- read any day number in integer and display day name in the word.c

---
+++ 136- read any day number in integer and display day name in the word.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void main()
+{
+   int dayno;
+   printf("enter a number of day");
+   scanf("%d",&dayno);
+   switch(dayno)
+   {
+	case 1:
+	       printf("monday\n");
+	       break;
+	case 2:
+	       printf("tuesday\n");
+	       break;
+	case 3:
+	       printf("wednesday\n");
+	       break;
+	case 4:
+	       printf("thursday\n");
+	       break;
+	case 5:
+	       printf("friday\n");
+	       break;
+	case 6:
+	       printf("saturday\n");
+	       break;
+	case 7:
+	       printf("sunday\n");
+	       break;
+	default:
+	       printf("invalid number");
+	       break;
+      }
+}
