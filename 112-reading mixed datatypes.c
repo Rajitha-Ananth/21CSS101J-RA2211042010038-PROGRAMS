c99986f171df3ecfa64a7a83e05b933e5f239a68
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-23 20:57:05 +0530

    Create 112-reading mixed datatypes.c

---
+++ 112-reading mixed datatypes.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main() {
+    int x;
+    char y;
+    float a;
+    printf("enter values of x,y,a:\n");
+    scanf("%d %c %f",&x,&y,&a);
+    printf("%d%c%f\n",x,y,a);
+    return 0;
+}
+    
