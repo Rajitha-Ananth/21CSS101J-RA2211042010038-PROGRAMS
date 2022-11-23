536a9a02a12bd6ed47809442fdc2b62a0d206404
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-24 01:39:54 +0530

    Create 134-whether a triangle can be formed by the given value for the angles.c

---
+++ 134-whether a triangle can be formed by the given value for the angles.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()  
+{  
+    int ang1,ang2,ang3,sum;   
+    printf("enter three angles of triangle:");  
+    scanf("%d%d%d",&ang1,&ang2,&ang3);  
+    sum =ang1+ ang2+ang3;   
+    if(sum==180)   
+    {  
+        printf("triangle is possible\n");  
+    }  
+    else  
+    {  
+        printf("triangle is not possible\n");  
+    }  
+ } 
