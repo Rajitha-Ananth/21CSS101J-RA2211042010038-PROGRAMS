933f90ccb2c8b1fecda18e3b3a498fe11bcec351
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-23 22:45:04 +0530

    Create 117-copy one string to another and count the number of characters.c

---
+++ 117-copy one string to another and count the number of characters.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main() {
+    char string1[100],string2[100];
+    int i;
+    printf("enter a string");
+    scanf("%s",string2);
+    for(i=0;string2[i]!='\0';i++) {
+        string1[i]=string2[i];
+        string1[i]='\0';
+        printf("%s\n",string1);
+    }
+    printf("number of characters =%d\n",i);
+}
