ced53cde0358d984559f8b3d0458e64e98c7d3a8
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-24 01:27:07 +0530

    Create 131-print a string in reverse order.c

---
+++ 131-print a string in reverse order.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+   char str1[100],tmp;
+   int l,a,b,i;
+   printf("enter a string:");
+   scanf("%s",&str1);
+   l=strlen(str1);
+   a=0;
+   b=l-1;
+   for(i=a;i<b;i++)
+   {
+       tmp=str1[i];
+       str1[i]=str1[b];
+       str1[b]=tmp;
+       b--;
+   }
+   printf("Reversed string is: %s\n",str1);
+}
