6beb053e6d1749e35696a69ad58edaea719f0b69
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-23 23:02:33 +0530

    Create 119-concatenate first,second,last name into single name.c

---
+++ 119-concatenate first,second,last name into single name.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+void main() {
+    int i,j,k;
+    char first_name[10];
+    char second_name[10];
+    char last_name[10];
+    char name[100];
+    for(i=0;first_name!='\0';i++) {
+        name[i]=first_name[i];
+    }
+    for(j=0;second_name!='\0';j++) {
+        name[i+j+1]=second_name[i+j+1];
+    }
+    for(k=0;last_name!='\0';k++) {
+        name[i+j+k+2]=last_name[i+j+k+2];
+    }
+    printf("%s\n",name);
+}
