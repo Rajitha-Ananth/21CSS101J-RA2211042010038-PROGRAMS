66b3ef9951befa158baef3d97db9e738d04f31f5
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-24 01:36:27 +0530

    Create 133-whether a triangle is Equilateral, Isosceles or Scalene.c

---
+++ 133-whether a triangle is Equilateral, Isosceles or Scalene.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()  {  
+    int sidea, sideb, sidec; 
+    printf("enter three sides of triangle: ");  
+    scanf("%d%d%d",&sidea,&sideb,&sidec);  
+    if(sidea==sideb && sideb==sidec)
+    {  
+        printf("equilateral triangle\n");  
+    }  
+    else if(sidea==sideb||sidea==sidec||sideb==sidec);
+    {  
+        printf("isosceles triangle\n");  
+    }  
+    else  
+    {  
+        printf("scalene triangle\n");  
+    }  
+    return 0;  
+} 
+
