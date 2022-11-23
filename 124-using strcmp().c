6fe19c3e66e4a6f9b53c717142c034ef16b650b4
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-24 00:16:50 +0530

    Create 124-using strcmp().c

---
+++ 124-using strcmp().c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+  char str1[] = "hello", str2[] = "world", str3[] = "hi";
+  int result;
+  result = strcmp(str1, str2);
+  printf("strcmp(str1, str2) = %d\n",result);
+  result = strcmp(str1, str3);
+  printf("strcmp(str1, str3) = %d\n",result);
+  return 0;
+}
