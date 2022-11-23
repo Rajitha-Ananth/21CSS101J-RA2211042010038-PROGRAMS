b72d7e49cefe88f07a8c5d976b7286ba57cc3eec
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-23 21:21:15 +0530

    Create 114-else if ladder on marks.c

---
+++ 114-else if ladder on marks.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main() {
+    float marks;
+    char grade;
+    printf("enter the marks obtained");
+    scanf("%f",&marks);
+    if(marks>=80 && marks<=100) {
+        grade="honours";
+    }
+    else if(marks>=60 && marks<=79) {
+        grade="first division";
+    }
+    else if(marks>=50 && marks<=59) {
+        grade="second division";
+    }
+    else if(marks>=20 && marks<=49) {
+        grade="third division";
+    }
+    else {
+        grade="fail";
+    }
+    printf("%s",grade);
+}
