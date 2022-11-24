878c197b8ac613bf473284d8e3c06c22d391b2f9
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-25 01:14:33 +0530

    Update 124-using strcmp().c

--- 124-using strcmp().c
+++ 124-using strcmp().c
@@ -3,6 +3,7 @@
 int main() {
   char str1[] = "hello", str2[] = "world", str3[] = "hi";
   int result;
+  printf("RA2211042010038\n");
   result = strcmp(str1, str2);
   printf("strcmp(str1, str2) = %d\n",result);
   result = strcmp(str1, str3);
