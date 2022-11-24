4ec1fdf024ff4bb2a1db06db9c194bbe35340771
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-24 21:58:04 +0530

    Create 144-Sorting names in ascending order.c

---
+++ 144-Sorting names in ascending order.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+ {
+ char name[30][20],t[20];
+ int i,j,n;
+ printf(“enter total number of names :”);
+ scanf(“%d”,&n);
+ printf(“enter the names\n”);
+ for(i=0;i<n;i++)
+ scanf(“%s”,name[i]);
+ for(i=0;i<n;i++)
+ for(j=i+1;j<n;j++)
+ if (strcmp(name[i],name[j])>0)
+ {
+ strcpy(t,name[i]);
+ strcpy(name[i],name[j]);
+ strcpy(name[i],t);
+ }
+ for(i=0;i<n;i++)
+ printf(“%s”,name[i]);
+ } 
