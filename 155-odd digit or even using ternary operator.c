8a79c669b97a8aaf42ce6b816121a43255bf5d37
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-25 00:17:16 +0530

    Create 155-odd digit or even using ternary operator.c

---
+++ 155-odd digit or even using ternary operator.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+	int n;
+	printf("n : ");
+	scanf("%d", &n);
+	(n%2==0) ? printf("%d is even\n",n) : printf("%d is odd\n",n); // Fill the missing code inside the parenthesis
+	return 0;
+}
