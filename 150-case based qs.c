97814d6cfc6eb6a5069cf805ebe8b4e1f0ef8a08
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-24 23:12:35 +0530

    Create 150-case based qs.c

---
+++ 150-case based qs.c
@@ -0,0 +1,14 @@
+/*Nancy bought apples in a fruit shop. The shop keeper specified the bill amount.
+Nancy also given some amount to the shop keeper for paying the bill. But she likes to know the quotient 
+and remainder after dividing the amount given by her by the bill amount specified by shop keeper.*/
+#include<stdio.h>
+int main() {
+	int a,b;
+	int c,d;
+	scanf("%d%d",&a,&b);
+	c=a%b;
+	d=a/b;
+	printf("%d\n",d);
+	printf("%d\n",c);
+}
+
