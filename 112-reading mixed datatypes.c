780f5f7b8d69bb525a475723f409967bb0a7f6d5
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-25 01:09:10 +0530

    Update 112-reading mixed datatypes.c

--- 112-reading mixed datatypes.c
+++ 112-reading mixed datatypes.c
@@ -3,6 +3,7 @@ void main() {
     int x;
     char y;
     float a;
+    printf("RA2211042010038\n");
     printf("enter values of x,y,a:\n");
     scanf("%d %c %f",&x,&y,&a);
     printf("%d%c%f\n",x,y,a);
