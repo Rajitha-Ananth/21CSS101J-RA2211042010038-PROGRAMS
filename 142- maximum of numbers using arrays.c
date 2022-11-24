07c724030c4fd1f749daa27f44cbe4203630e036
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-24 21:45:39 +0530

    Create 142- maximum of numbers using arrays.c

---
+++ 142- maximum of numbers using arrays.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+ {
+ int a[50],n,i,max;
+ printf("enter the value of : ");
+ scanf("%d",&n);
+ printf("enter the numbers\n");
+ for(i=1;i<=n;i++) {
+     scanf("%d",&a[i]);
+     max=a[1];
+     for(i=2;i<=n;i++) {
+         if (max<a[i])
+         max=a[i];
+     }
+ }
+ printf("Minimum = %d",max);
+ } 
