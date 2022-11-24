225b7c65d316e852085307975e633327529e1c83
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-25 01:30:46 +0530

    Update 148-case based- total number of hours and minutes the train was delayed.c

--- 148-case based- total number of hours and minutes the train was delayed.c
+++ 148-case based- total number of hours and minutes the train was delayed.c
@@ -1,14 +1,12 @@
 /*Binita was travelling from Chennai to Delhi in Rajdhani Express. 
-
 The train have arrived at the destination later than the estimated time. 
-
 So, Binita wants to know the total number of hours and minutes the train was delayed.
-
 Can you help Binita in finding the exact hour and time Rajdhani Express was delay on the day of Binita's journey?
 */
 #include<stdio.h>
 void main() {
 	int min;
+	printf("RA2211042010038\n");
 	scanf("%d",&min);
 	printf("%d Hours and %d Minutes",min/60,min%60);
 }
