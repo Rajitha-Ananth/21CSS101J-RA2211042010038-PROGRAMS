adec8f0b693a4ad82299d646c351f285c33e0463
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-25 01:31:19 +0530

    Update 149-case based on grid.c

--- 149-case based on grid.c
+++ 149-case based on grid.c
@@ -1,10 +1,10 @@
 /*Tina's brother gave her a friendly task of calculating the number of squares in a board that has n*n squares of dimensions 1cm *1cm each.
-
 Help her to find the number of total squares including all small and big ones.*/
 
 #include<stdio.h>
 int main() {
 	int n,a;
+	printf("RA2211042010038\n");
 	scanf("%d",&n);
 	a=(n*(n+1)*(2*n+1))/6;
 	printf("%d",a);
