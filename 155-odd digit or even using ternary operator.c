220ea9f34e80cf8d5fbdf89d4498d95f95a284c5
Rajitha <113624471+Rajitha-Ananth@users.noreply.github.com>
2022-11-25 01:36:11 +0530

    Update 155-odd digit or even using ternary operator.c

--- 155-odd digit or even using ternary operator.c
+++ 155-odd digit or even using ternary operator.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main() {
 	int n;
+	printf("RA2211042010038\n");
 	printf("n : ");
 	scanf("%d", &n);
 	(n%2==0) ? printf("%d is even\n",n) : printf("%d is odd\n",n); // Fill the missing code inside the parenthesis
